8df27729f8efd495561655850f8ba2f1d6fa3990
Shuvodip Barua <baruashuvodip@gmail.com>
2026-09-23 16:07:04 -0500

    working on python differentiation. library function can't do higher orders!!

--- derivative_recursion.py
+++ derivative_recursion.py
@@ -1,3 +1,5 @@
+from scipy.differentiate import derivative
+
 def centralDiff(f, x, h, n=1):
     result = 0.0
     if (n > 1):
@@ -6,3 +8,20 @@ def centralDiff(f, x, h, n=1):
     else:
         result = (f(x + h/2)- f(x- h/2))/h
     return result
+
+x = float(input("Enter x: "))
+
+def f(x):
+    return 3*x**2 - 26
+
+
+h = 0.0001
+
+order_num = int(input("Enter order of derivative: "))
+
+
+print("Our Result =", centralDiff(f,x,h,order_num))
+#print("Library function Result =", derivative(f,x,order = 4))
+library_result = derivative(f,x, order = 2).df
+
+print(library_result)
